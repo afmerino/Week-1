3b182dc3f6daf0b93550e8a7a1bdeed6e6ad7a78
Amaris Merino <51328772+afmerino@users.noreply.github.com>
2019-06-25 15:18:30 -0400

    Add files via upload

---
+++ week 1/web/practice.cs
@@ -0,0 +1,10 @@
+driving_age = 16
+permit_age = 15
+my_age = 28
+if my_age >= driving_age:
+print('you can drive')
+elif my_age == permit_age:
+print('with a parent you can drive')
+elsE:
+print('NO')
+you can drive 
